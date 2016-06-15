badb258f4638269b37f846a563758cd1a5f88e4f
Coen Jacobs <coenjacobs@gmail.com>
2016-06-15 15:58:06 +0200

    Moved widgets over from core plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,10 +2,15 @@
 
 namespace Listings\Jobs;
 
+use Listings\Jobs\Widgets\FeaturedJobs;
+use Listings\Jobs\Widgets\RecentJobs;
+
 class Plugin {
     public function __construct() {
         // Switch theme
         add_action( 'after_switch_theme', 'flush_rewrite_rules', 15 );
+
+        add_action( 'widgets_init', array( $this, 'widgets_init' ) );
 
         // Actions
         add_action( 'after_setup_theme', array( $this, 'load_plugin_textdomain' ) );
@@ -14,5 +19,13 @@ class Plugin {
     public function load_plugin_textdomain() {
         load_textdomain( 'listings-jobs', WP_LANG_DIR . "/listings-jobs/listings-jobs-" . apply_filters( 'plugin_locale', get_locale(), 'listings-jobs' ) . ".mo" );
         load_plugin_textdomain( 'listings-jobs', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    }
+
+    /**
+     * Widgets init
+     */
+    public function widgets_init() {
+        register_widget( RecentJobs::class );
+        register_widget( FeaturedJobs::class );
     }
 }
