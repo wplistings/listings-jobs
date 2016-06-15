97183b3a04e5b6e1d6b1ce1ba55052e345efd347
Coen Jacobs <coenjacobs@gmail.com>
2016-06-15 19:55:58 +0200

    Jobs specific writepanels implementation class

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -2,10 +2,13 @@
 
 namespace Listings\Jobs\Admin;
 
+use Listings\Jobs\Admin\Writepanels\JobDetails;
+
 class Admin
 {
     public function __construct()
     {
+        $this->jobdetails = new JobDetails();
         $this->cpt = new Cpt();
     }
 }
