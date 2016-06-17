363752daf4dd1b9389a16fb876298947bf474107
Coen Jacobs <coenjacobs@gmail.com>
2016-06-17 17:20:56 +0200

    Load generated css files in right places

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -10,5 +10,11 @@ class Admin
     {
         $this->jobdetails = new JobDetails();
         $this->cpt = new Cpt();
+
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_scripts'));
+    }
+
+    public function enqueue_scripts() {
+        wp_enqueue_style( 'listings-jobs-admin', LISTINGS_JOBS_PLUGIN_URL . '/assets/css/admin.css' );
     }
 }
