110c6d548182effa43bfbe22f7dc11f7a5fccaaa
Coen Jacobs <coenjacobs@gmail.com>
2016-07-07 14:31:13 +0200

    Fixed localised variable name in job dash js

--- assets/js/job-dashboard.js
+++ assets/js/job-dashboard.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
 	$('.job-dashboard-action-delete').click(function() {
-		return confirm( job_manager_job_dashboard.i18n_confirm_delete );
+		return confirm( listings_job_dashboard.i18n_confirm_delete );
 	});
 
 });
