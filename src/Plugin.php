02d8be01107278cbde5b46f583290035bcf858c9
Coen Jacobs <coenjacobs@gmail.com>
2016-07-07 14:29:35 +0200

    Added job-dashboard js to this plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -88,5 +88,11 @@ class Plugin {
         wp_register_script( 'listings-ajax-filters', LISTINGS_JOBS_PLUGIN_URL . '/assets/js/ajax-filters.min.js', $ajax_filter_deps, LISTINGS_JOBS_VERSION, true );
         wp_localize_script( 'listings-ajax-filters', 'listings_ajax_filters', $ajax_data );
         wp_enqueue_script( 'listings-job-application', LISTINGS_JOBS_PLUGIN_URL . '/assets/js/job-application.min.js', array( 'jquery' ), LISTINGS_JOBS_VERSION, true );
+
+        wp_register_script( 'listings-job-dashboard', LISTINGS_JOBS_PLUGIN_URL . '/assets/js/job-dashboard.min.js', array( 'jquery' ), LISTINGS_JOBS_VERSION, true );
+        wp_localize_script( 'listings-job-dashboard', 'listings_job_dashboard', array(
+            'i18n_confirm_delete' => __( 'Are you sure you want to delete this listing?', 'listings' )
+        ) );
+        wp_enqueue_script( 'listings-job-dashboard');
     }
 }
