4bf680840870dd8e52eb82fc3e8f3f48dcec2abc
Coen Jacobs <coenjacobs@gmail.com>
2016-07-08 11:25:33 +0200

    Enqueue admin js with localised date format

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -19,5 +19,10 @@ class Admin
 
     public function enqueue_scripts() {
         wp_enqueue_style( 'listings-jobs-admin', LISTINGS_JOBS_PLUGIN_URL . '/assets/css/admin.css' );
+        wp_enqueue_script( 'listings-jobs-admin', LISTINGS_JOBS_PLUGIN_URL. '/assets/js/admin.min.js', array( 'jquery', 'jquery-tiptip', 'jquery-ui-datepicker' ), LISTINGS_VERSION, true );
+
+        wp_localize_script( 'listings-jobs-admin', 'listings_jobs_admin', array(
+            'date_format' => _x( 'yy-mm-dd', 'Date format for jQuery datepicker', 'listings-jobs' )
+        ) );
     }
 }
